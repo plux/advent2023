1dd004284b5afc577ec79a425c14dcaebfaba9e3
Hakan Nilsson <haakan@gmail.com>
2023-12-02 19:47:38 +0100

    Day 2, simpler parsing

--- src/day2.erl
+++ src/day2.erl
@@ -3,47 +3,34 @@
 -include("aoc.hrl").
 
 solve(Input) ->
-   {part1(?lines(Input)), part2(?lines(Input))}.
+   {part1(parse(Input)), part2(parse(Input))}.
 
 part1(Lines) ->
-    Games = [parse_line(Line) || Line <- Lines],
-    lists:sum([Id || {Id, Game} <- Games, is_possible(Game)]).
+    lists:sum([Id || {Id, Game} <- Lines, is_possible(Game)]).
 
 part2(Lines) ->
-    Games = [parse_line(Line) || Line <- Lines],
-    lists:sum([power(Game) || {_Id, Game} <- Games]).
+    lists:sum([power(Game) || {_, Game} <- Lines]).
+
+parse(Input) ->
+    [parse_line(Line) || Line <- ?lines(Input)].
 
 parse_line(Line) ->
-    [IdStr, GameStr] = ?split(Line, ":"),
-    [Id] = ?ints(IdStr),
-    Game = lists:map(
-             fun(Sub) ->
-                     Cubes = [?split(string:strip(Cube), " ") ||
-                                 Cube <- ?split(Sub, ",")],
-                     [{Color, ?int(Num)} || [Num, Color] <- Cubes]
-             end, ?split(GameStr, ";")),
-    {Id, Game}.
+    [_, Id | G] = re:split(Line, "[,;: ]", [{return, list}]),
+    Chunks = ?chunks([X || X <- G, X /= ""], 2),
+    Game = [{?int(N), C} || [N, C] <- Chunks],
+    {?int(Id), Game}.
 
 is_possible(Game) ->
     NumCubes = #{"red" => 12, "green" => 13, "blue" => 14},
-    lists:all(fun(SubGame) ->
-                       lists:all(fun ({Color, N}) ->
-                                         N =< maps:get(Color, NumCubes, 0)
-                                 end, SubGame)
+    lists:all(fun({N, Color}) ->
+                      N =< maps:get(Color, NumCubes, 0)
               end, Game).
-
 power(Game) ->
-    Maxes = lists:foldl(
-              fun(SubGame, Acc) ->
-                      lists:foldl(
-                        fun({Color, N}, SubAcc) ->
-                                Max = max(maps:get(Color, SubAcc, 0), N),
-                                SubAcc#{Color => Max}
-                        end, Acc, SubGame)
-              end, #{}, Game),
+    Maxes = lists:foldl(fun({N, Color}, Acc) ->
+                                Max = max(maps:get(Color, Acc, 0), N),
+                                Acc#{Color => Max}
+                        end, #{}, Game),
     lists:foldl(fun erlang:'*'/2, 1, maps:values(Maxes)).
-
-
 
 -include_lib("eunit/include/eunit.hrl").
 solve_test_() ->
